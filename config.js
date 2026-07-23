056d64f3ebd5489b59fda6d52464261a4b4fba0f
snowyang1217 <snowyang1217@gmail.com>
2026-07-24 07:00:44 +0800

    Update LIFF ID

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
 
     // 你的 LIFF ID
-    LIFF_ID: "2010246946-LqKHYAMc",
+    LIFF_ID: "2010246946-BKtkjkvh",
 
     // Make Webhook
-    WEBHOOK_URL: "https://hook.us2.make.com/y43cmdoesh6nbpur4owkxupekeh6g122/xxxxxxxxxxxxxxxx"
+    WEBHOOK_URL: "https://hook.us2.make.com/y43cmdoesh6nbpur4owkxupekeh6https://hook.us2.make.com/y43cmdoesh6nbpur4owkxupekeh6g122g122/xxxxxxxxxxxxxxxx"
 
 };
