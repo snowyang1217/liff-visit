a35fbac90651f9479641b9d4400a06d6a6ba6946
snowyang1217 <snowyang1217@gmail.com>
2026-07-27 10:39:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const CONFIG = {
     LIFF_ID: "2010246946-BKtkjkvh",
 
     // Make Webhook
-    WEBHOOK_URL: "https://hook.us2.make.com/w9n1tlhvajhl33w4zii8cjseji5z9isf"
+    WEBHOOK_URL: "https://hook.us2.make.com/pquw6gsrvtd1c937lljbguq5xh6y7o7a"
 
 };
